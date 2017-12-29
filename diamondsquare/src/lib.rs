49ca6202069c02c96c2566ac7da2a47146db709d
Sébastien Watteau <swatteau@gmail.com>
2017-12-29 14:55:24 +0100

    Added Random trait

--- diamondsquare/src/lib.rs
+++ diamondsquare/src/lib.rs
@@ -3,19 +3,24 @@
 #[derive(Debug, Clone, Copy)]
 pub struct Position(pub usize, pub usize);
 
-#[derive(Debug)]
-pub struct Terrain {
+pub trait Random {
+    fn random(&mut self, p: Position, unit: usize) -> f64;
+}
+
+pub struct Terrain<'a> {
     order: u32,
     size: usize,
+    rand: Option<&'a mut Random>,
     values: Vec<f64>,
 }
 
-impl Terrain {
-    pub fn new(order: u32) -> Terrain {
+impl<'a> Terrain<'a> {
+    pub fn new(order: u32) -> Terrain<'a> {
         let size = 2usize.pow(order) + 1;
         Terrain {
             order: order,
             size: size,
+            rand: None,
             values: vec![0f64; size * size],
         }
     }
@@ -49,35 +54,50 @@ impl Terrain {
     }
 
     fn square_step(&mut self, p: Position, unit: usize) {
-        let Position(col, row) = p;
-        let mut value = 0f64;
-        value += self.get(Position(col - unit, row - unit));
-        value += self.get(Position(col - unit, row + unit));
-        value += self.get(Position(col + unit, row - unit));
-        value += self.get(Position(col + unit, row + unit));
-        *self.get_mut(p) += value / 4.0;
+        let average_part = {
+            let Position(col, row) = p;
+            let mut value = 0f64;
+            value += self.get(Position(col - unit, row - unit));
+            value += self.get(Position(col - unit, row + unit));
+            value += self.get(Position(col + unit, row - unit));
+            value += self.get(Position(col + unit, row + unit));
+            value / 4.0
+        };
+
+        *self.get_mut(p) += average_part + self.get_random_part(p, unit);
     }
 
     fn diamond_step(&mut self, p: Position, unit: usize) {
-        let Position(col, row) = p;
-        let (mut neighbors, mut value) = (0, 0f64);
-        if col >= unit {
-            value += self.get(Position(col - unit, row));
-            neighbors += 1;
+        let average_part = {
+            let Position(col, row) = p;
+            let (mut neighbors, mut value) = (0, 0f64);
+            if col >= unit {
+                value += self.get(Position(col - unit, row));
+                neighbors += 1;
+            }
+            if col + unit < self.size {
+                value += self.get(Position(col + unit, row));
+                neighbors += 1;
+            }
+            if row >= unit {
+                value += self.get(Position(col, row - unit));
+                neighbors += 1;
+            }
+            if row + unit < self.size {
+                value += self.get(Position(col, row + unit));
+                neighbors += 1;
+            }
+            value / neighbors as f64
+        };
+
+        *self.get_mut(p) += average_part + self.get_random_part(p, unit);
+    }
+
+    fn get_random_part(&mut self, p: Position, unit: usize) -> f64 {
+        match self.rand {
+            Some(ref mut rand) => rand.random(p, unit),
+            None => 0.0,
         }
-        if col + unit < self.size {
-            value += self.get(Position(col + unit, row));
-            neighbors += 1;
-        }
-        if row >= unit {
-            value += self.get(Position(col, row - unit));
-            neighbors += 1;
-        }
-        if row + unit < self.size {
-            value += self.get(Position(col, row + unit));
-            neighbors += 1;
-        }
-        *self.get_mut(p) += value / neighbors as f64;
     }
 
     fn get(&self, p: Position) -> f64 {
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,4 @@ use diamondsquare::Terrain;
 fn main() {
     let mut terrain = Terrain::new(5);
     terrain.generate(1.0, 10.0, 20.0, 40.0);
-    println!("{:#?}", terrain);
 }
