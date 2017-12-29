39d0816270b031919bfb4e801c30c2cb7f7d7715
Sébastien Watteau <swatteau@gmail.com>
2017-12-29 21:17:39 +0100

    Added python viewer

---
+++ viewer.py
@@ -0,0 +1,44 @@
+import sys
+import math
+from tkinter import Tk, Canvas, PhotoImage, mainloop
+
+def hexcolor(r, g, b):
+    return '#%02x%02x%02x' % (r, g, b)
+
+def main(args):
+
+    path = "/home/sebastien/terrain.dat"
+    with open(path, 'rb') as f:
+        bytes = f.read()
+
+    size = int(math.sqrt(len(bytes)))
+    window = Tk()
+    canvas = Canvas(window, width=size, height=size, bg="#000000")
+    canvas.pack()
+    img = PhotoImage(width=size, height=size)
+    canvas.create_image((size/2, size/2), image=img, state="normal")
+
+    b = 0
+    for y in range(size):
+        for x in range(size):
+            h = bytes[b]
+            b+=1
+            if h < 60:
+                img.put(hexcolor(0, 0, h), (x, y))
+            elif h == 60:
+                img.put(hexcolor(int(0.5*h), int(0.5*h), h), (x, y))
+            elif h == 61:
+                img.put(hexcolor(h, h, h), (x, y))
+            elif h == 62:
+                img.put(hexcolor(int(2*h), int(1.5*h), int(0.85*h)), (x, y))
+            else:
+                if h % 10 == 0:
+                    #img.put(hexcolor(140,80,80), (x, y))
+                    img.put(hexcolor(int(0.2*h), int(0.4*h), 0), (x, y))
+                else:
+                    img.put(hexcolor(int(0.25*h), int(0.5*h), 0), (x, y))
+
+    mainloop()
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
