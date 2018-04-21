2439e60b7c07f8f060559f9f221c34c8e1895939
Sébastien Watteau <swatteau@gmail.com>
2018-04-21 15:01:02 +0200

    Fixed deleted use statement

--- diamondsquare/src/extrinsic.rs
+++ diamondsquare/src/extrinsic.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.use super::{ExtrinsicFn, Position};
 
+use super::{ExtrinsicFn, Position};
 use rand;
 
 pub struct Null;
